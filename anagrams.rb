cc65d77c3e96fc063a611d90f445657f8c8c2b37
Abraham <abraham.eleno@gmail.com>
2016-10-12 11:14:53 -0500

    Versión extendida

--- anagrams.rb
+++ anagrams.rb
@@ -1,3 +1,7 @@
+# escribe un método llamado are_anagrams? que reciba dos palabras, 
+# sin importar si existen en un diccionario, y devuelva true o false.
+# Intenta escribir un método llamado canonical que reciba una palabra y la regrese 
+# en un formato especifico el cual, si comparas dos anagramas después de mandados a este método sean iguales.
 def canonical(word)
   word.chars.sort
 end
@@ -9,3 +13,15 @@ end
 # p canonical("omar")
 p are_anagrams?("roma", "mora")
 p are_anagrams?("mono", "nomo")
+
+# crear un método que se llame anagrams_for el cual deberá recibir una palabra y un arreglo de palabras (un diccionario) 
+# y regresar un arreglo de las palabras que sean anagramas de esa palabra o un arreglo vació si no encuentra ninguna.
+
+dictionary = ["gema", "amor", "hongo", "ella", "roma", "mora", "dedo", "mira", "fila", "omar"]
+
+def anagrams_for(word, array)
+	word = canonical(word)
+		array.select {|wrd| wrd = canonical(wrd) == word}
+end
+
+p anagrams_for("roma", dictionary) == ["amor", "roma", "mora", "omar"]
